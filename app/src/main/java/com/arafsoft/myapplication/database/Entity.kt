73881c38af066077f6742c82dc4876d865bc7055
alireza <alirezaaf12@outlook.com>
2022-08-14 19:13:00 +0430

    initial

---
+++ app/src/main/java/com/arafsoft/myapplication/database/Entity.kt
@@ -0,0 +1,7 @@
+package com.arafsoft.myapplication.database
+
+import androidx.room.Entity
+
+@Entity(tableName = "music_table")
+class Entity {
+}
---
+++ app/src/main/java/com/arafsoft/myapplication/database/MusicDao.kt
@@ -0,0 +1,7 @@
+package com.arafsoft.myapplication.database
+
+import androidx.room.Dao
+
+@Dao
+class MusicDao {
+}
