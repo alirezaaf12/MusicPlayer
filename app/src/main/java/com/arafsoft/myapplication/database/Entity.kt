5f383000007581d7386bbd1abdfe0c55dcd895dd
alireza <alirezaaf12@outlook.com>
2022-08-14 22:33:02 +0430

    initial

--- app/src/main/java/com/arafsoft/myapplication/database/Entity.kt
+++ app/src/main/java/com/arafsoft/myapplication/database/Entity.kt
@@ -1,7 +1,7 @@
 package com.arafsoft.myapplication.database
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "music_table")
-class Entity {
-}
+class Music (@PrimaryKey val id: Int, var title: String, var path: String)
--- app/src/main/java/com/arafsoft/myapplication/database/MusicDao.kt
+++ app/src/main/java/com/arafsoft/myapplication/database/MusicDao.kt
@@ -1,7 +1,22 @@
 package com.arafsoft.myapplication.database
 
-import androidx.room.Dao
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-class MusicDao {
+interface MusicDao {
+    @Query("SELECT * FROM music_table ORDER BY id ASC")
+    fun getAllMusic(): Flow<List<Music>>
+
+    @Query("SELECT * FROM music_table WHERE id = :id")
+    suspend fun getMusicById(id: Int): Flow<Music>
+
+    @Insert
+    suspend fun insertMusic(vararg music: Music)
+
+    @Delete
+    suspend fun deleteMusic(music: Music)
+
+    @Update
+    suspend fun updateMusic(music: Music)
 }
